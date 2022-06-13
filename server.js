bba30b134b8748304f6e652d204de34c6655d948
sdmnny <sdmnny@gmail.com>
2022-06-13 15:00:28 -0400

    deleted querys.sql

--- server.js
+++ server.js
@@ -16,3 +16,20 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the movies_db database.')
 );
+
+// GET /api/movies 
+app.get (`/api/movies`, (req, res) => {
+    db.query(`SELECT * FROM movies`, function (err, results)
+   {
+    console.log(results);
+   })
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+
+});
